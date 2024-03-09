8c7ecc0845cb16fbdb9e45925fb2e6599d72ae5f
PaoCorona <paopcorona@gmail.com>
2024-03-08 18:46:56 -0600

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dados';
+  dadoIzq= '../assets/img/dice1.png';
+  dadoDer= '../assets/img/dice4.png';
+
+  num1: number =0;
+  num2: number =0;
+
+  tirarDados(): void
+    {
+      this.num1=Math.round(Math.random()*5)+1;
+      this.num2=Math.round(Math.random()*5)+1;
+      this.dadoIzq='../assets/img/dice' + this.num1 + '.png';
+      this.dadoDer='../assets/img/dice' + this.num2 + '.png';
+    }
 }
